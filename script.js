0ac5d0daa048a352596a7e54b0581ba7376c7d53
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-21 02:30:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,92 @@
-var style = document.createElement('style');
-style.innerHTML = `
-[id^="visual"] {
-height: 0;
-padding-bottom: 66.67%;
-background-position: center;
-background-repeat: no-repeat;
-background-size: cover;
-cursor: ew-resize;
-position: relative;
-filter: brightness(125%) contrast(90%) saturate(100%) blur(0px) hue-rotate(0deg);
-}
+document.addEventListener('DOMContentLoaded', function() {
+  var style = document.createElement('style');
+  style.innerHTML = `
+    [id^="visual"] {
+        height: 0;
+        padding-bottom: 66.67%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        cursor: ew-resize;
+        position: relative;
+        filter: brightness(125%) contrast(90%) saturate(100%) blur(0px) hue-rotate(0deg);
+    }
 
-@media (max-width: 767px) {
-[id^="visual"] {
-max-width: 100%;
-padding-bottom: 66.67%;
-}
-}
+    @media (max-width: 767px) {
+        [id^="visual"] {
+            max-width: 100%;
+            padding-bottom: 66.67%;
+        }
+    }
 
-[id^="visual"] {
-padding: 31%;
-}
-`;
-document.head.appendChild(style);
+    [id^="visual"] {
+        padding: 31%;
+    }
+  `;
+  document.head.appendChild(style);
 
-function fetchDataAndHandleEvents(elementId, jsonUrl) {
-const element = document.getElementById(elementId);
-const totalImages = 48;
-let currentImage = 1;
+  function fetchDataAndHandleEvents(elementId, jsonUrl) {
+    const element = document.getElementById(elementId);
+    const totalImages = 48;
+    let currentImage = 1;
 
-fetch(jsonUrl)
-.then(response => response.json())
-.then(data => {
-const images = data.images;
+    fetch(jsonUrl)
+      .then(response => response.json())
+      .then(data => {
+        const images = data.images;
 
-element.style.backgroundImage = `url('${images[currentImage - 1]}')`;
+        element.style.backgroundImage = `url('${images[currentImage - 1]}')`;
 
-let isMouseDown = false;
-let startX;
+        let isMouseDown = false;
+        let startX;
 
-function updateImage() {
-element.style.backgroundImage = `url('${images[currentImage - 1]}')`;
-}
+        function updateImage() {
+          element.style.backgroundImage = `url('${images[currentImage - 1]}')`;
+        }
 
-function handleStart(e) {
-isMouseDown = true;
-startX = e.clientX || e.touches[0].clientX;
-}
+        function handleStart(e) {
+          isMouseDown = true;
+          startX = e.clientX || e.touches[0].clientX;
+        }
 
-function handleMove(e) {
-if (!isMouseDown) return;
+        function handleMove(e) {
+          if (!isMouseDown) return;
 
-const x = e.clientX || e.touches[0].clientX;
-const dx = x - startX;
-if (Math.abs(dx) > 5) {
-if (dx < 0) {
-currentImage++;
-if (currentImage > totalImages) currentImage = 1;
-} else {
-currentImage--;
-if (currentImage < 1) currentImage = totalImages;
-}
+          const x = e.clientX || e.touches[0].clientX;
+          const dx = x - startX;
+          if (Math.abs(dx) > 5) {
+            if (dx < 0) {
+              currentImage++;
+              if (currentImage > totalImages) currentImage = 1;
+            } else {
+              currentImage--;
+              if (currentImage < 1) currentImage = totalImages;
+            }
 
-updateImage();
-startX = x;
-}
-}
+            updateImage();
+            startX = x;
+          }
+        }
 
-function handleEnd() {
-isMouseDown = false;
-}
+        function handleEnd() {
+          isMouseDown = false;
+        }
 
-element.addEventListener('mousedown', handleStart);
-element.addEventListener('mousemove', handleMove);
-element.addEventListener('mouseup', handleEnd);
-element.addEventListener('mouseleave', handleEnd);
+        element.addEventListener('mousedown', handleStart);
+        element.addEventListener('mousemove', handleMove);
+        element.addEventListener('mouseup', handleEnd);
+        element.addEventListener('mouseleave', handleEnd);
 
-element.addEventListener('touchstart', handleStart);
-element.addEventListener('touchmove', handleMove);
-element.addEventListener('touchend', handleEnd);
-})
-.catch(error => {
-console.error(`Terjadi kesalahan dalam mengambil data JSON (${elementId}):`, error);
+        element.addEventListener('touchstart', handleStart);
+        element.addEventListener('touchmove', handleMove);
+        element.addEventListener('touchend', handleEnd);
+      })
+      .catch(error => {
+        console.error(`Terjadi kesalahan dalam mengambil data JSON (${elementId}):`, error);
+      });
+  }
+
+  fetchDataAndHandleEvents('visual1', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/streetfighterv4sp2black.json');
+  fetchDataAndHandleEvents('visual2', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/streetfighterv4sblack.json');
+  fetchDataAndHandleEvents('visual3', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/panigalev4r.json');
 });
-}
-
-fetchDataAndHandleEvents('visual1', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/streetfighterv4sp2black.json');
-fetchDataAndHandleEvents('visual2', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/streetfighterv4sblack.json');
-fetchDataAndHandleEvents('visual3', 'https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2/panigalev4r.json');
