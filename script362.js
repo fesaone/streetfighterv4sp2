8fbc86f5535f248328f2575bc573d39fee1239a7
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-20 13:36:53 +0700

    Create script362.js

---
+++ script362.js
@@ -0,0 +1,59 @@
+const image362 = document.getElementById('image362');
+  const totalImages = 48;
+  let currentImage = 1;
+
+  // Mengambil data dari file JSON
+  fetch('https://cdn.jsdelivr.net/gh/fesaone/streetfighterv4sp2@main/streetfighter.json')
+    .then(response => response.json())
+    .then(data => {
+      const images = data.images;
+
+      image362.style.backgroundImage = `url('${images[currentImage - 1]}')`;
+
+      let isMouseDown = false;
+      let startX;
+
+      function updateImage() {
+          image362.style.backgroundImage = `url('${images[currentImage - 1]}')`;
+      }
+
+      function handleStart(e) {
+          isMouseDown = true;
+          startX = e.clientX || e.touches[0].clientX;
+      }
+
+      function handleMove(e) {
+          if (!isMouseDown) return;
+
+          const x = e.clientX || e.touches[0].clientX;
+          const dx = x - startX;
+          if (Math.abs(dx) > 5) {
+              if (dx < 0) {
+                  currentImage++;
+                  if (currentImage > totalImages) currentImage = 1;
+              } else {
+                  currentImage--;
+                  if (currentImage < 1) currentImage = totalImages;
+              }
+
+              updateImage();
+              startX = x;
+          }
+      }
+
+      function handleEnd() {
+          isMouseDown = false;
+      }
+
+      image362.addEventListener('mousedown', handleStart);
+      image362.addEventListener('mousemove', handleMove);
+      image362.addEventListener('mouseup', handleEnd);
+      image362.addEventListener('mouseleave', handleEnd);
+
+      image362.addEventListener('touchstart', handleStart);
+      image362.addEventListener('touchmove', handleMove);
+      image362.addEventListener('touchend', handleEnd);
+    })
+    .catch(error => {
+      console.error('Terjadi kesalahan dalam mengambil data JSON:', error);
+    });
